2ef040b2d52f9ea9ad628eba1c43b26fc37d0f34
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-02-21 23:19:06 +0000

    Updated schedules

--- c3d3/c3d3/definitions/whole_market_trades_history/definitions.py
+++ c3d3/c3d3/definitions/whole_market_trades_history/definitions.py
@@ -3,7 +3,7 @@ from dagster import Definitions, AssetsDefinition
 from c3d3.assets.whole_market_trades_history.assets import get_overview
 from c3d3.ops.whole_market_trades_history.ops import extract_from_c3vault, load_to_dwh
 from c3d3.jobs.whole_market_trades_history.jobs import dag
-from c3d3.schedules.whole_market_trades_history.schedules import every_30th_minute
+from c3d3.schedules.whole_market_trades_history.schedules import every_15th_minute
 from c3d3.resources.c3vault.resource import c3vault
 from c3d3.resources.logger.resource import logger
 from c3d3.resources.dwh.resource import dwh
@@ -27,5 +27,5 @@ whole_market_trades_history = Definitions(
         'df_serializer': df_serializer,
         'w3sleep': w3sleep
     },
-    schedules=[every_30th_minute]
+    schedules=[every_15th_minute]
 )
--- c3d3/c3d3/schedules/bids_and_asks/schedules.py
+++ c3d3/c3d3/schedules/bids_and_asks/schedules.py
@@ -3,8 +3,8 @@ from dagster import ScheduleDefinition
 from c3d3.jobs.bids_and_asks.jobs import dag
 
 
-every_30th_minute = ScheduleDefinition(
+every_15th_minute = ScheduleDefinition(
     name='bids_and_asks',
     job=dag,
-    cron_schedule="*/30 * * * *"
+    cron_schedule="*/15 * * * *"
 )
