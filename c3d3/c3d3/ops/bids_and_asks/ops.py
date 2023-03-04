5da859a1781bc71272288078271d29e7bfdc789a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-03-04 14:19:35 +0000

    Updated load_to_dwh ops

--- c3d3/c3d3/ops/bids_and_asks/ops.py
+++ c3d3/c3d3/ops/bids_and_asks/ops.py
@@ -89,7 +89,7 @@ def load_to_dwh(context, df: List[list]) -> None:
     for mini_df in df:
         mini_df = context.resources.df_serializer.df_from_list(mini_df)
         concat_df = concat_df.append(mini_df, ignore_index=True)
-        context.resources.logger.info(mini_df.info())
+        context.resources.logger.info(mini_df.dtypes)
     context.resources.dwh.load(
         table='pit_big_table_bids_and_asks',
         df=concat_df
