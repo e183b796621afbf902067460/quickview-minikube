28f27dd8279ebc9e663b79af85c4712cbc1eb02a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-03-08 14:38:46 +0000

    Updated whole_market_trades_history schedule

--- c3d3/c3d3/schedules/whole_market_trades_history/schedules.py
+++ c3d3/c3d3/schedules/whole_market_trades_history/schedules.py
@@ -3,8 +3,8 @@ from dagster import ScheduleDefinition
 from c3d3.jobs.whole_market_trades_history.jobs import dag
 
 
-every_10th_minute = ScheduleDefinition(
+every_5th_minute = ScheduleDefinition(
     name='whole_market_trades_history',
     job=dag,
-    cron_schedule="*/10 * * * *"
+    cron_schedule="*/5 * * * *"
 )
