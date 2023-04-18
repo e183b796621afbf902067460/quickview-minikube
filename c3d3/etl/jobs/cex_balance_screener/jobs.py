d00b7b0584682361c34a9f1ce6d3dbf54790cdd5
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-04-18 07:47:27 +0000

    Added cex_balance_screener DAG

---
+++ c3d3/etl/jobs/cex_balance_screener/jobs.py
@@ -0,0 +1,46 @@
+from dagster import job
+from dagster_aws.s3.io_manager import s3_pickle_io_manager
+from dagster_aws.s3.resources import s3_resource
+import os
+
+from etl.executors.celery.executor import celery_executor
+from etl.assets.cex_balance_screener.assets import get_overview
+from etl.ops.cex_balance_screener.ops import extract_from_c3vault, load_to_dwh
+from etl.resources.c3vault_exposure.resource import c3vault_exposure
+from etl.resources.logger.resource import logger
+from etl.resources.dwh.resource import dwh
+from etl.resources.fernet.resource import fernet
+from etl.resources.serializers.resource import df_serializer
+
+
+@job(
+    name='cex_screener',
+    resource_defs={
+        'c3vault_exposure': c3vault_exposure,
+        'dwh': dwh,
+        'logger': logger,
+        'fernet': fernet,
+        'df_serializer': df_serializer,
+        "io_manager": s3_pickle_io_manager,
+        "s3": s3_resource
+    },
+    executor_def=celery_executor,
+    config={
+        'resources': {
+            'io_manager': {
+                'config': {
+                    's3_bucket': os.getenv('S3_BUCKET', None)
+                }
+            },
+            's3': {
+                'config': {
+                    'endpoint_url': os.getenv('S3_BACKEND_URL', None)
+                }
+            }
+        }
+    }
+)
+def dag():
+    configs = extract_from_c3vault()
+    overviews = configs.map(get_overview)
+    load_to_dwh(overviews.collect())
